163d31081b68d5cebb78f2ee692f7ca8106e27cf
qzmp <lukasz.golebiowski@hotmail.com>
2018-03-03 04:49:46 +0100

    Revert "Merge branch 'master' of https://github.com/qzmp/TKGJ2018"
    This reverts commit 85d8c83f42631f1a98b9c6edf49a56913ced5812, reversing
    changes made to 02c37f17a5d1d0091abafed0d0a2999e041a5002.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour {
     public float horizontalSpeed = 5;
 
 	// Referencja do kamery
+	public GameObject cameraObject;
 
 	// Flagi koloru wiatru
 	private bool Red;
@@ -38,14 +39,13 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
 		// Przemieszczanie gracza wzgledem pozycji myszki
-		Vector3 playerPosition = Camera.main.WorldToScreenPoint(GetComponent<Transform>().position);
+		Vector3 playerPosition = cameraObject.GetComponent<Camera>().WorldToScreenPoint(GetComponent<Transform>().position);
 		Debug.Log("target: " + playerPosition.y + ", mouse is: " + Input.mousePosition);
 		float moveVertical = 0;
-		if ((Input.mousePosition.y > playerPosition.y+1*speed) && (playerPosition.y < Camera.main.pixelHeight))
+		if (Input.mousePosition.y > playerPosition.y)
 			moveVertical = 1;
-		else if ((Input.mousePosition.y < playerPosition.y-1*speed) && (playerPosition.y > 0))
+		else if (Input.mousePosition.y < playerPosition.y)
 			moveVertical = -1;
-<<<<<<< HEAD
 		Vector3 movement = new Vector3 (horizontalSpeed, moveVertical * verticalSpeed, 0.0f);
 		GetComponent<Rigidbody>().velocity = movement;
 
@@ -65,13 +65,6 @@ public class PlayerController : MonoBehaviour {
             SetWindColor();
         }
     }
-=======
-		Vector3 movement = new Vector3 (0.0f, moveVertical, 0.0f);
-
-			GetComponent<Rigidbody>().velocity = movement * speed;
-
-	}
->>>>>>> 4551b61cb0feb3227eaf03bb45c777d05e931a5a
 
 	void FixedUpdate ()
 	{
