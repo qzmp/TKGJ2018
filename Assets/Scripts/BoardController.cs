805a76ffdae848476371b3b8ce670b28bbd45f0e
qzmp <lukasz.golebiowski@hotmail.com>
2018-03-03 03:45:58 +0100

    Spawning and Destroying

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    public GameObject barrierPrefab;
+    public GameObject player;
+    public float startWait = 5;
+    public float spawnWait = 5;
+    public float spawnWaitRandomPercent = 0.3f;
+
+    public Vector3 spawnLocation;
+
+    
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(player.transform.position.x + spawnLocation.x, Random.Range(-spawnLocation.y, spawnLocation.y), spawnLocation.z);
+            Instantiate(barrierPrefab, spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject prefab;
+    private GameObject player;
+    public float startWait = 0;
+    public float spawnWait = 3;
+    public float spawnWaitRandomPercent = 0.3f;
+    
+    public float spawnX = 12;
+    public float spawnYMin = 0;
+    public float spawnYMax = 0;
+    public float spawnZMin = 0;
+    public float spawnZMax = 0;
+    
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(player.transform.position.x + spawnX, Random.Range(spawnYMin, spawnYMax), Random.Range(spawnZMin, spawnZMax));
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+            Instantiate(prefab, spawnPosition, rotation);
+            yield return new WaitForSeconds(spawnWait + Random.Range(-spawnWait * spawnWaitRandomPercent, spawnWait * spawnWaitRandomPercent));
+        }
+    }
+}
