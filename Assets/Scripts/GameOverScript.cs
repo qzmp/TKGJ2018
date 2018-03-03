a4e9e6372b9efe2a5e9b604b45f9e16c22c19166
MDikkii <mdemkow@gmail.com>
2018-03-03 12:13:43 +0100

    Added game over popup and next level popup

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class GameOverScript : MonoBehaviour
+    {
+        private PlayerController playerController;
+
+        public GameObject GameOverPanel;
+
+        // Use this for initialization
+        void Start()
+        {
+            playerController = FindObjectOfType<PlayerController>();
+            GameOverPanel.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (playerController.hp <= 0)
+            {
+                GameOverPanel.SetActive(true);
+                //save score?
+            }
+        }
+
+        public void ExitToMainMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void ReloadLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/NextLevelScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class NextLevelScript : MonoBehaviour
+    {
+        private PlayerController playerController;
+
+        public GameObject NextLevelPanel;
+
+        // Use this for initialization
+        void Start()
+        {
+            playerController = FindObjectOfType<PlayerController>();
+            NextLevelPanel.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // TODO: add end level condition
+            if (false)
+            {
+                NextLevelPanel.SetActive(true);
+                //save score?
+            }
+        }
+
+        public void ExitToMainMenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void NextLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
