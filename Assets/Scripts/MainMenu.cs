4de607c5660340adc236693f4d780f5aace622ed
MDikkii <mdemkow@gmail.com>
2018-03-03 04:35:26 +0100

    Added menu and credits

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private const string ScoreSceneName = "ScoreScene";
+    private const string CreditsSceneName = "CreditsScene";
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartCredits()
+    {
+        SceneManager.LoadScene(CreditsSceneName);
+    }
+
+    public void ShowScoreBoard()
+    {
+        SceneManager.LoadScene(ScoreSceneName);
+    }
+}
