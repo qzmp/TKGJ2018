9f9c08ffc3bc7b72d5642c8e9e534d91ba1d7af6
MDikkii <mdemkow@gmail.com>
2018-03-04 08:48:26 +0100

    Spawner to cloud

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,11 +6,20 @@ public class Spawner : MonoBehaviour {
 
     public GameObject[] prefabs;
     public Vector3 location;
+    public bool isCloud;
 
 	// Update is called once per frame
-	public void SpawnNew () {
-        Vector3 spawnPosition = new Vector3(transform.position.x + location.x, location.y, location.z);
-        Quaternion rotation = Quaternion.Euler(new Vector3(0, UnityEngine.Random.Range(0, 360), 0));
+	public void SpawnNew ()
+	{
+
+	    var y = location.y;
+	    if (isCloud)
+	    {
+	        y = UnityEngine.Random.Range(location.y-1, location.y+1);
+	    }
+
+        Vector3 spawnPosition = new Vector3(transform.position.x + location.x, y, location.z);
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, !isCloud ? UnityEngine.Random.Range(0, 360) : 0, 0));
         GameObject newObject = Instantiate(prefabs[UnityEngine.Random.Range(0, prefabs.Length - 1)], spawnPosition, rotation);
         newObject.GetComponent<SpecialDestroyer>().spawner = this;
         //newObject.transform.localScale = new Vector3(3, 3, 3);
