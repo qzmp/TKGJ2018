a729d6495370fcf8d0f9426304d0199384e2448a
qzmp <lukasz.golebiowski@hotmail.com>
2018-03-20 12:20:36 +0100

    Background scrolling by texture move

---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour {
+
+    private Vector2 uvOffset = Vector2.zero;
+    public Vector2 uvAnimationRate = new Vector2(0.5f, 0);
+
+    private Renderer renderer;
+
+
+	void Start () {
+        renderer = GetComponent<Renderer>();
+	}
+
+	void Update () {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if(renderer.enabled)
+        {
+            renderer.material.SetTextureOffset("_MainTex", uvOffset);
+        }
+	}
+}
